cae7302ef4d84a0b35e507d82a2abdaf875e3ddf
Revensif <revensif@mail.ru>
2023-11-24 14:54:34 +0300

    removed Consts and Configs classes

--- src/main/java/edu/messenger/Database/Configs.java
+++
@@ -1,9 +0,0 @@
-package edu.messenger.Database;
-
-public class Configs {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPassword = "12345";
-    protected String dbName = "revensif";
-}
--- src/main/java/edu/messenger/Database/Consts.java
+++
@@ -1,9 +0,0 @@
-package edu.messenger.Database;
-
-public class Consts {
-    public static final String USER_TABLE = "users";
-    public static final String USERS_ID = "id_users";
-    public static final String USERS_NICKNAME = "nickname";
-    public static final String USERS_PASSWORD = "password";
-    public static final String USERS_LOCATION = "location";
-}
